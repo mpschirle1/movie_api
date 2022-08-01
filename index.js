e6781ad1e4fbdddcfba3ff574a2ee1d475345d94
Matt <mpschirle1@gmail.com>
2022-07-31 22:57:24 -0700

    Connected DB to API on Heroku

--- index.js
+++ index.js
@@ -26,7 +26,9 @@ require('./passport');
 
 const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {flags: 'a'})
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.connect( process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true});
 
 app.use(morgan('combined', {stream: accessLogStream}));
 app.use(express.static('public'));
